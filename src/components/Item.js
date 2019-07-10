771fc3ad8782d8e6d286a38615f483c6aa8d56da
nguyentruong <nguyentruong.xyz0405@gmail.com>
2019-07-10 17:07:11 +0700

    fixShadow

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Image, Text } from 'react-native'
 
+import CardView from 'react-native-cardview'
+
 import Star from './Star'
 
 import ImageFood from '../assets/images/Image.png'
 
 export default class Item extends Component {
   render() {
+    
+
     return (
-      <View style={styles.itemSection}>
-        <Image source={ImageFood} style={styles.image} />
-        <View>
-          <Text style={styles.textTitle}>Hummingbied</Text>
-          <View style={styles.vote}>
-            <Star />
-            <Text style={styles.voteNumber}>35</Text>
+      
+
+
+        <View style={styles.itemSection}>
+          <Image source={ImageFood} style={styles.image} />
+          <View>
+            <Text style={styles.textTitle}>Hummingbied</Text>
+            <View style={styles.vote}>
+              <Star />
+              <Text style={styles.voteNumber}>35</Text>
+            </View>
+            <Text style={styles.textAddress}>Address</Text>
           </View>
-          <Text style={styles.textAddress}>Address</Text>
         </View>
-      </View>
+      
     )
   }
 }
@@ -28,13 +36,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 16,
     borderRadius: 4,
-    backgroundColor: 'green',
+    backgroundColor: 'orange',
     shadowColor: 'black',
     // shadowRadius: 10,
     shadowOpacity: 0.3,
     shadowOffset: { width: 10, height: 10 },
     marginBottom: 16,
-    flexDirection: 'row'
+    flexDirection: 'row',
+    shadowColor: "#000",
+    shadowOffset: {
+	width: 0,
+	height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+
+elevation: 24,
   },
   image: {
     borderRadius: 500,
